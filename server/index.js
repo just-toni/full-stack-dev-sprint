3d40ad0890d671f2e79a32b28e34344f06901699
just-toni <75427368+just-toni@users.noreply.github.com>
2024-02-22 16:44:41 -0400

    weird

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000
+
+app.connect(MONGO_URL).then(() => console.log("Connected successfully")).catch(
+    (err) => console.error("Could connect to database ... try again", err)
+);
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})
