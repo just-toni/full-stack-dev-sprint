8db7c45aefd717b1b3a043da5cc99330bff8b8b0
just-toni <75427368+just-toni@users.noreply.github.com>
2024-02-22 17:01:35 -0400

    i know day 2 on day 1 fixing it

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000
 
-app.connect(MONGO_URL).then(() => console.log("Connected successfully")).catch(
+mongoose.connect(MONGO_URL).then(() => console.log("Connected successfully")).catch(
     (err) => console.error("Could connect to database ... try again", err)
 );
+
+app.use(express)
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
